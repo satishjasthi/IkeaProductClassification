bb6b43561ed55d1a14c9fbc650dd02a7af3f3a74
satishjasthi <satish27may@gmail.com>
2019-10-09 02:07:44 +0530

    Add model info to readme

--- README.md
+++ README.md
@@ -1,19 +1,56 @@
 # IkeaProductClassification
 Project to identify products from IKEA product categories.
 
-This project has data scraped from IKEA on four different classes:
+## Data
 
-- **Chairs**
+- This project has data scraped from IKEA on four different classes:
 
-![Chairs](https://i.imgur.com/Aa1D8ME.png)
+    - **Chairs**
+    
+    ![Chairs](https://i.imgur.com/Aa1D8ME.png)
+    
+    - **Beds**
+    ![Beds](https://i.imgur.com/AayLhPC.png)
+    
+    - **Candle Holders**
+    ![Candle Holders](https://i.imgur.com/MUkcW3a.png)
+    
+    - **Wardrobes**
+    ![Wardrobes](https://i.imgur.com/OPx8SNS.png)
 
-- **Beds**
-![Beds](https://i.imgur.com/AayLhPC.png)
 
-- **Candle Holders**
-![Candle Holders](https://i.imgur.com/MUkcW3a.png)
+- There are total 2,421 product images in data/model_data directory. However,
+Images scrapped alone from IKEA website (using src/FetchData.py) for each product category is 
+not enough to train a multiclass classifier using a pretrained model.
+- Hence additional data scraped from google images  has been added 
+to raise number of samples per class at least to 400 images(Images from
+google images were downloaded using [Download all Images](https://chrome.google.com/webstore/detail/download-all-images/nnffbdeachhbpfapjklmpnmjcgamcdmm?hl=en)
+ chrome extension).
+- Scrapped data is split into train and validation datasets where
+validation data has atleast 40 samples per each class
 
-- **Wardrobes**
-![Wardrobes](https://i.imgur.com/OPx8SNS.png)
+## Training a multi-class classifier
+- ResNext_50(R50) pretrained is used as a classifier.
+- For transfer learning, the ImageNet specific FC layer of the R50 is removed and replaced
+with the following layers 
+    - AdaptiveAvgPool2d
+    - AdaptiveMaxPool2d
+    - Flatten
+    - BatchNorm1d
+    - Dropout(p=0.25)
+    - FC (512 units)
+    - ReLU
+    - BatchNorm1d
+    - Dropout(p=0.5)
+    - FC(4 units)
+- R50 is trained for 20 epochs using one cycle learning rate policy
 
+## Training and Validation loss
+![Training and Validation loss](https://i.imgur.com/8NPrwwM.png)
 
+## Confusion matrix
+![confusion matrix](https://i.imgur.com/iOg8h8v.png)
+
+## Interpreting model predictions
+
+![Predictions](https://i.imgur.com/GQ3o2ks.png)
--- src/FetchData.py
+++ src/FetchData.py
@@ -6,7 +6,6 @@ About: Script to scrape data from IKEA.com for different product categories
 
 Author: Satish Jasthi
 """
-import sys
 from time import sleep
 import urllib.request
 
@@ -27,20 +26,21 @@ class DataScraper:
                              'candle_holders': f'{self.home_page}/candle-holders-candles-10760/?page=20'
                              }
         # xpath of image in product page
-        self.image_xpath = """//*[@id="content"]/div[4]/div[2]/div/div/div[2]/div[1]/div[1]/div/div[*]/div/div/a[1]/div/div/div/img"""
+        self.image_xpath = """//*[@id="content"]/div[4]/div[2]/div/div/
+        div[2]/div[1]/div[1]/div/div[*]/div/div/a[1]/div/div/div/img"""
         self.data_dir = data_dir
 
-    def downloadProductImages(self, productName:str):
+    def download_product_images(self, product_name: str):
         """
-        Method to scrape and download images related to given productName
-        :param productName: str, name of product category
+        Method to scrape and download images related to given product_name
+        :param product_name: str, name of product category
         :return: None
         """
-        product_dir = self.data_dir/f'{productName}'
+        product_dir = self.data_dir / f'{product_name}'
         product_dir.mkdir(parents=True, exist_ok=True)
 
         # load product page in browser
-        self.browser.get(self.product_urls[productName])
+        self.browser.get(self.product_urls[product_name])
         sleep(10)
 
         # scrolling web page from top to bottom multiple times to ensure images are loaded
@@ -52,26 +52,27 @@ class DataScraper:
         assert len(image_xpaths) > 1, f'Number of image_xpaths == {len(image_xpaths)}'
 
         # downloading images
-        print(f'Downloading {productName} images...............')
-        for indx,image_xpath in tqdm(enumerate(image_xpaths)):
-            img_path = product_dir/f'{product_dir.name}_{indx}.jpeg'
+        print(f'Downloading {product_name} images...............')
+        for indx, image_xpath in tqdm(enumerate(image_xpaths)):
+            img_path = product_dir / f'{product_dir.name}_{indx}.jpeg'
             try:
-                urllib.request.urlretrieve(image_xpath.get_attribute('src'),img_path.as_posix())
+                urllib.request.urlretrieve(image_xpath.get_attribute('src'), img_path.as_posix())
             except Exception as e:
                 print(f'Failed downloading image\nError:{e}')
                 print('\n Trying to download after 5 secs')
                 sleep(5)
                 urllib.request.urlretrieve(image_xpath.get_attribute('src'), img_path.as_posix())
 
-    def scrapeProductsData(self):
+    def scrape_products_data(self):
         """
         Method to scrape all product images
         :return: None
         """
         for product in self.product_urls.keys():
-            self.downloadProductImages(productName=product)
+            self.download_product_images(product_name=product)
         self.browser.close()
 
 
-o = DataScraper()
-o.scrapeProductsData()
+if __name__ == "__main__":
+    o = DataScraper()
+    o.scrape_products_data()
